6d96bd48457f60cb19ed54cec92055df6b046da8
Trojanzaro <anastasiszaro@gmail.com>
2026-03-06 12:27:35 +0200

    misc

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -456,8 +456,8 @@ routerAdd("POST", "/_dist/process_payment", async (httpContext) => {
         const charger_tin = httpContext.requestInfo().body.charger_tin;
 
         //also get AADE api keys from env variables
-        const aade_username = $env.get("AADE_API_KEY");
-        const aade_password = $env.get("AADE_API_SECRET");
+        const aade_username = $os.getenv("AADE_API_KEY");
+        const aade_password = $os.getenv("AADE_API_SECRET");
 
         console.log("Processing payment with amount: "+amount+" and charger TIN: "+charger_tin);
 
