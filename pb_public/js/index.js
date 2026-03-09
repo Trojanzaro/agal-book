8d6ab6f2eb69f0f8a02547b42fdc0e7f3416c75b
Trojanzaro <anastasiszaro@gmail.com>
2026-03-09 15:56:22 +0200

    misc fix

--- pb_public/js/index.js
+++ pb_public/js/index.js
@@ -55,7 +55,7 @@ function selectStudentProfile(studentId, displayName) {
     // set header
     document.getElementById('profile_name').innerHTML = pb.authStore.model['auth_type'] === 'student' ? displayName : '<a href="javascript:studentDetails(\'' + studentId + '\');sidebarNavActive(\'students\');">' + displayName + '</a>';
     document.getElementById('profile_student_id').innerText = 'ID: ' + studentId;
-    document.getElementById('addGradeBtn').setAttribute('style', 'display: block;');
+    document.getElementById('addGradeBtn').setAttribute('style', 'display:' + (pb.authStore.model['auth_type'] !== 'student' ? 'inline-block' : 'none'));
     // store selected id
     document.getElementById('grade_student_id').value = studentId;
     // load assignments
