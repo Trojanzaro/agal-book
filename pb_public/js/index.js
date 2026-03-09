d6950cb67f37c2d582a2f9526c17799e6e5a7cba
Trojanzaro <anastasiszaro@gmail.com>
2026-03-09 19:55:28 +0200

    more tests

--- pb_public/js/index.js
+++ pb_public/js/index.js
@@ -107,31 +107,30 @@ async function loadStudentProfile(studentId) {
     });
     drawChart(subs)
 }
+
 function drawChart(grades) {
-console.log('Drawing chart with grades:', grades);
-// fix these so that it uses the promises .then() method like the other times that google charts were used int the file
-  google.charts.load('current', {'packages':['corechart']}).then(() => {
-    google.charts.setOnLoadCallback(drawChart);
-    const data = new google.visualization.DataTable();
-    data.addColumn('string', 'Type');
-    data.addColumn('number', 'Average Grade');
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Type');
+  data.addColumn('number', 'Average Grade');
 
-    data.addRows([
-        ['Tests', calculateAverage(grades.test)],
-        ['Quizzes', calculateAverage(grades.quiz)],
-        ['Homework', calculateAverage(grades.homework)]
-    ]);
+  data.addRows([
+    ['Tests', calculateAverage(grades.filter(g => g.type === 'test').map(g => parseFloat(g.grade)))],
+    ['Quizzes', calculateAverage(grades.filter(g => g.type === 'quiz').map(g => parseFloat(g.grade)))],
+    ['Homework', calculateAverage(grades.filter(g => g.type === 'homework').map(g => parseFloat(g.grade)))]
+  ]);
 
-    const options = {
-        title: 'Grade Distribution',
-        is3D: true,
-        vAxis: {title: 'Average Grade'},
-        hAxis: {title: 'Grade Type'},
-    };
+  const options = {
+    title: 'Grade Distribution',
+    is3D: true,
+    vAxis: { title: 'Average Grade' },
+    hAxis: { title: 'Grade Type' },
+  };
 
-    const chart = new google.visualization.AreaChart(document.getElementById('areaChart'));
-    chart.draw(data, options);
-  });
+  const chart = new google.visualization.AreaChart(
+    document.getElementById('areaChart')
+  );
+
+  chart.draw(data, options);
 }
 
 function calculateAverage(grades) {
